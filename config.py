18a74633fba48f86241ccb4f676e683797988369
Will3201 <quloquadrato@gmail.com>
2026-08-06 04:59:28 +0200

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# CONFIGURAZIONE DEL BOT
-# IMPORTANTE: usa un repository GitHub PRIVATO.
-
-TELEGRAM_TOKEN = "8670212259:AAFn_21_abtz4vL4WQ5TpekYby-hCnAjzeU"
-CANALE_CHAT_ID = "@TarloDelRisparmio"
-AMAZON_TAG = "tarlodelrispa-21"
-ADMIN_SECRET = "Caccamolle12*"
